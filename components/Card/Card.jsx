69acd0df50d273e6aa29241d4ccd6d1638b8d4b7
alicenyberg <alice.nyberg@hotmail.com>
2022-10-26 11:39:59 +0200

    more card styling

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -13,39 +13,47 @@ const CardWrapper = styled.div`
   align-items: center;
   flex-direction: column;
   margin: 3rem;
+  font-family: "RedHatDisplay";
 
   @media screen and (min-width: 768px) {
-    height: 390px;
-    width: 260px;
+    height: 430px;
+    width: 270px;
   }
 `;
 
-const StyledProductImg = styled.img`
+const TextWrapper = styled.div`
+  height: 40%;
+`;
+
+const StyledProductImg = styled.div`
   width: 105px;
   height: 105px;
+  position: relative;
+  margin: 1rem;
   @media screen and (min-width: 768px) {
-    width: 192px;
-    height: 192px;
+    width: 180px;
+    height: 180px;
   }
 `;
 
 const ProductTitle = styled.p`
   font-size: 18px;
   margin: 0;
-  margin-top: 0.2rem;
+  text-transform: uppercase;
+  text-align: center;
 
   @media screen and (min-width: 768px) {
     font-size: 24px;
-    margin-top: 0.4rem;
   }
 `;
 
 const ProductPrice = styled.p`
-  font-size: 12px;
+  font-size: 10px;
   margin: 0;
+  text-align: center;
 
   @media screen and (min-width: 768px) {
-    font-size: 16px;
+    font-size: 14px;
   }
 `;
 
@@ -58,22 +66,75 @@ const ProductText = styled.p`
 
   @media screen and (min-width: 768px) {
     font-size: 14px;
-    margin-top: 0.4rem;
-    padding: 0.8rem;
+    padding: 1rem;
+    padding-top: 0.7rem;
+    padding-bottom: 0.7rem;
+    margin: 0;
+  }
+`;
+
+const ProductSize = styled.div`
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    text-align: center;
+    margin: 0;
   }
 `;
 
 function Card() {
   return (
-    <CardWrapper>
-      <StyledProductImg src="/produkt.png"></StyledProductImg>
-      <ProductTitle>MINI</ProductTitle>
-      <ProductPrice>750 KR</ProductPrice>
-      <ProductText>
-        Enfärgad ring gjord av sterlingsilver - fylld med resin. Ca 25 mm i
-        diameter.
-      </ProductText>
-    </CardWrapper>
+    <div>
+      <CardWrapper>
+        <StyledProductImg>
+          <Image src="/produkt.png" layout="fill"></Image>
+        </StyledProductImg>
+        <TextWrapper>
+          <ProductTitle>MIDI</ProductTitle>
+          <ProductPrice>750 KR</ProductPrice>
+          <ProductText>
+            Enfärgad ring gjord av sterlingsilver - fylld med resin.
+          </ProductText>
+          <ProductSize>
+            Mått: Ca 25 mm i diameter. Justerbar ringstomme.
+          </ProductSize>
+        </TextWrapper>
+      </CardWrapper>
+      <CardWrapper>
+        <StyledProductImg>
+          <Image src="/produkt.png" layout="fill"></Image>
+        </StyledProductImg>
+        <TextWrapper>
+          <ProductTitle>Oxiderade cirkelörhängen</ProductTitle>
+          <ProductPrice>450kr/500kr/550kr</ProductPrice>
+          <ProductText>
+            Örhängen med stift gjorda av oxiderat sterlingsilver.
+          </ProductText>
+          <ProductSize>
+            Mått: Välj mellan 13 mm, 17 mm, 20 mm. Justerbar ringstomme.
+          </ProductSize>
+        </TextWrapper>
+      </CardWrapper>
+      <CardWrapper>
+        <StyledProductImg>
+          <Image src="/produkt.png" layout="fill"></Image>
+        </StyledProductImg>
+        <TextWrapper>
+          <ProductTitle>Assymetriska</ProductTitle>
+          <ProductPrice>
+            650kr/750kr/850kr/<br></br>950kr/1050kr
+          </ProductPrice>
+          <ProductText>
+            Enfärgade assymetriska ringar gjorda av sterlingsilver - fyllda med
+            resin.
+          </ProductText>
+          <ProductSize>
+            Mått: Finns i storlekarna Mini, Midi, Maxi, Mega & OOmega.
+          </ProductSize>
+        </TextWrapper>
+      </CardWrapper>
+    </div>
   );
 }
 export default Card;
